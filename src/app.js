fd80b47f37abd1b2be1f5630d794afd469dae706
ingdev8023 <144868641+ingdev8023@users.noreply.github.com>
2023-11-15 21:55:19 +0000

    proyecto Terminado

--- src/app.js
+++ src/app.js
@@ -5,7 +5,44 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+const random = arr => {
+  let randomNumber = Math.floor(Math.random() * (arr.length - 1));
+  return arr[randomNumber];
+};
+
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let suits = ["♦", "♥", "♠", "♣"];
+  let numbers = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+  let suit = "";
+  let button = document.querySelector("#generator");
+  let cardSuitIcon = document.querySelectorAll(".suitIcon");
+  let cardNumber = document.querySelector("#number");
+  let parentCard = document.querySelector(#parentCard);
+  button.addEventListener("click", () => {
+    suit = random(suits);
+    cardSuitIcon.forEach(a => {
+      a.textContent = `${suit}`;
+      suit === "♦" || suit === "♥"
+        ? (a.style.color = "red")
+        : (a.style.color = "black");
+    });
+    cardNumber.textContent = `${random(numbers)}`;
+  });
+
+  setTimeout(() => {
+    suit = random(suits);
+    cardSuitIcon.forEach(a => {
+      a.textContent = `${suit}`;
+      suit === "♦" || suit === "♥"
+        ? (a.style.color = "red")
+        : (a.style.color = "black");
+    });
+    cardNumber.textContent = `${random(numbers)}`;
+  }, 5000);
+
+
+
+
+  
 };
